6c78a80076b0ac099da657ad7076eecc921a88df
EvaGLN <maeva.galiano@gmail.com>
2026-07-25 21:47:43 +0200

    first commit

---
+++ _print_decimal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _print_decimal - prints a decimal
+ * @args: arguments
+ * Return: number of characters printed
+ */
+int _print_decimal(va_list args)
+{
+	int integer, count = 0;
+	unsigned int unsinteger, div = 1;
+	char c;
+	
+	integer = va_arg(args, int);
+	if (integer == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	else if (integer < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		unsinteger = -(unsigned int)integer;
+	}
+	else
+		unsinteger = (unsigned int)integer;
+	while ((unsinteger / div) >= 10)
+		div *= 10;
+	while (div > 0)
+	{
+		c = (unsinteger / div) + '0';
+		write(1, &c, 1);
+		count++;
+		unsinteger %= div;
+		div /= 10;
+	}
+	return (count);
+}
